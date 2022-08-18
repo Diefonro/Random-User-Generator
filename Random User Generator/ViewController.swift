7e13c21f54871b035fe7d0615700c96052961171
Diego Fonseca <dfonsecar@unal.edu.co>
2022-08-18 15:32:52 -0300

    Initial commit
    Files added and initial view created

---
+++ Random User Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random User Generator
+//
+//  Created by Andrés Fonseca on 18/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
