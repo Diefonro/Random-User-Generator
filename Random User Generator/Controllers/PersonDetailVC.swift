8b58bfca39812a59a4c3f1fd65e1cb16242935ec
Diego Fonseca <dfonsecar@unal.edu.co>
2022-08-20 15:43:25 -0300

    Revert "updated person's info titles"
    This reverts commit 0cbea49100895c27286403fed659430697a6efb0.

--- Random User Generator/Controllers/PersonDetailVC.swift
+++ Random User Generator/Controllers/PersonDetailVC.swift
@@ -12,15 +12,10 @@ class PersonDetailVC: UIViewController {
     @IBOutlet var pullView: UIView?
     @IBOutlet var lblPersonName: UILabel?
     @IBOutlet var personImage: UIImageView?
-    @IBOutlet var lblPersonGenderTitle: UILabel?
     @IBOutlet var lblPersonGender: UILabel?
     @IBOutlet var lblPersonBirthdate: UILabel?
-    @IBOutlet var lblPersonBirthdateTitle: UILabel?
-    @IBOutlet var lblPersonEmailTitle: UILabel?
     @IBOutlet var lblPersonEmail: UILabel?
-    @IBOutlet var lblPersonPhoneTitle: UILabel?
     @IBOutlet var lblPersonPhone: UILabel?
-    @IBOutlet var lblPersonAddressTitle: UILabel?
     @IBOutlet var lblPersonAddress: UILabel?
     
     var user: User? {
@@ -40,15 +35,10 @@ class PersonDetailVC: UIViewController {
         lblPersonName?.text = "\(user!.name?.first ?? "") \(user!.name?.last ?? "")"
         personImage?.kf.setImage(with: URL(string: user?.picture?.large ?? ""))
         lblPersonEmail?.text = user?.email
-        lblPersonEmailTitle?.text = "\(user!.name?.first ?? "")'s email"
         lblPersonPhone?.text = user?.phone
-        lblPersonPhoneTitle?.text = "\(user!.name?.first ?? "")'s phone"
-        lblPersonGender?.text = user?.gender?.capitalized
-        lblPersonGenderTitle?.text = "\(user!.name?.first ?? "")'s gender"
+        lblPersonGender?.text = user?.gender
         lblPersonAddress?.text = "\(user!.location?.street?.name ?? "") \(user!.location?.street?.number ?? 0), \(user!.location?.city ?? ""), \(user!.location?.state ?? ""), \(user!.location?.country ?? "")"
-        lblPersonAddressTitle?.text = "\(user!.name?.first ?? "")'s address"
         lblPersonBirthdate?.text = "\(user!.dob?.date ?? Date()),  \(user!.dob?.age ?? 0) years old."
-        lblPersonBirthdateTitle?.text = "\(user!.name?.first ?? "")'s birthdate"
         pullView?.layer.cornerRadius = 5
     }
     
