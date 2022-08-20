d6c249691aaaca63e89d31f963af120a432e770e
Diego Fonseca <dfonsecar@unal.edu.co>
2022-08-20 15:58:56 -0300

    updated person info's labels
    titles bring first name and added constraint to address label to breakline when overflow

--- Random User Generator/Controllers/PersonDetailVC.swift
+++ Random User Generator/Controllers/PersonDetailVC.swift
@@ -13,10 +13,15 @@ class PersonDetailVC: UIViewController {
     @IBOutlet var lblPersonName: UILabel?
     @IBOutlet var personImage: UIImageView?
     @IBOutlet var lblPersonGender: UILabel?
+    @IBOutlet var lblPersonGenderTitle: UILabel?
     @IBOutlet var lblPersonBirthdate: UILabel?
+    @IBOutlet var lblPersonBirthdateTitle: UILabel?
     @IBOutlet var lblPersonEmail: UILabel?
+    @IBOutlet var lblPersonEmailTitle: UILabel?
     @IBOutlet var lblPersonPhone: UILabel?
+    @IBOutlet var lblPersonPhoneTitle: UILabel?
     @IBOutlet var lblPersonAddress: UILabel?
+    @IBOutlet var lblPersonAddressTitle: UILabel?
     
     var user: User? {
         didSet {
@@ -33,13 +38,17 @@ class PersonDetailVC: UIViewController {
     
     func updateInfo(){
         lblPersonName?.text = "\(user!.name?.first ?? "") \(user!.name?.last ?? "")"
-        personImage?.kf.setImage(with: URL(string: user?.picture?.large ?? ""))
-        lblPersonEmail?.text = user?.email
-        lblPersonPhone?.text = user?.phone
-        lblPersonGender?.text = user?.gender
-        lblPersonAddress?.text = "\(user!.location?.street?.name ?? "") \(user!.location?.street?.number ?? 0), \(user!.location?.city ?? ""), \(user!.location?.state ?? ""), \(user!.location?.country ?? "")"
-        lblPersonBirthdate?.text = "\(user!.dob?.date ?? Date()),  \(user!.dob?.age ?? 0) years old."
-        pullView?.layer.cornerRadius = 5
+                personImage?.kf.setImage(with: URL(string: user?.picture?.large ?? ""))
+                lblPersonEmail?.text = user?.email
+                lblPersonEmailTitle?.text = "\(user!.name?.first ?? "")'s email"
+                lblPersonPhone?.text = user?.phone
+                lblPersonPhoneTitle?.text = "\(user!.name?.first ?? "")'s phone"
+                lblPersonGender?.text = user?.gender?.capitalized
+                lblPersonGenderTitle?.text = "\(user!.name?.first ?? "")'s gender"
+                lblPersonAddress?.text = "\(user!.location?.street?.name ?? "") \(user!.location?.street?.number ?? 0), \(user!.location?.city ?? ""), \(user!.location?.state ?? ""), \(user!.location?.country ?? "")"
+                lblPersonAddressTitle?.text = "\(user!.name?.first ?? "")'s address"
+                lblPersonBirthdate?.text = "\(user!.dob?.date ?? Date()),  \(user!.dob?.age ?? 0) years old."
+                lblPersonBirthdateTitle?.text = "\(user!.name?.first ?? "")'s birthdate"
     }
     
 }
